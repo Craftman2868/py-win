36dbcb0a3b6b98e4f1a2a5de3a22abfb756912c8
Craftman2868 <mateo.leblois@gmail.com>
2021-02-11 13:29:42 +0100

    MAJ de l'exemple

--- example/main.py
+++ example/main.py
@@ -4,6 +4,7 @@ class App(App):
     def run(self):
         mainInterface = self.get_interface("main")
         win = self.get_window(mainInterface)
+        
         win.open()
-    def script_test(self, window, widget):
-        print("aaa")
+    def command_test(self, window, widget):
+        widget.set("text", "abc")
