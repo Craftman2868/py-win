5d6a963f0adf88a9fd00963e5581fef04183e2f7
Craftman2868 <mateo.leblois@gmail.com>
2021-02-15 17:09:53 +0100

    Création d'un code minimal

---
+++ minimal_code/main.py
@@ -0,0 +1,9 @@
+from pyWin import App
+
+class App(App):
+    def run(self):
+        enLang = self.get_lang("en")
+        mainInterface = self.get_interface("main", enLang)
+        mainWindow = self.create_window(mainInterface)
+
+        mainWindow.open()
